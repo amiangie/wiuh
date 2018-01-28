20e25d47acabc510c036528b6b3fc5b3e68033e1
Angie K. Titova <angie.badfox@gmail.com>
2018-01-28 20:27:44 +0200

    [Scripts + Styles] Generate slider dots based on slides number, display slider title

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -3,6 +3,19 @@
 const carousel = function (ele) {
   const carouselEle = document.querySelector(ele);
   const carousel = new Wallop(carouselEle);
+  const carouselItems = carousel.allItemsArray;
+  const dotsHolder = document.querySelector('.js-dots');
+  const slideTitle = document.querySelector('.js-title');
+  
+  carouselItems.forEach((item, i) => {
+    const dotTemplate = `
+      <button class="c-carousel__dot js-dot">
+        <span class="u-visuallyhidden">Go to item ${i + 1}</span>
+      </button>
+    `;
+    dotsHolder.insertAdjacentHTML('beforeend', dotTemplate);
+  });  
+  
   const dots = [...carouselEle.querySelectorAll('.js-dot')];
   
   dots.forEach((dot, i) => {
@@ -14,9 +27,12 @@ const carousel = function (ele) {
       dot.classList.remove('-active');
     });
     dots[event.detail.currentItemIndex].classList.add('-active');
+    
+    const title = document.querySelector('.js-item.Wallop-item--current').getAttribute('data-title');
+    
+    slideTitle.innerHTML = title;
   });
   
-  console.log(carousel)
 };
 
 module.exports = carousel;
