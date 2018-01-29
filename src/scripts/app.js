9439a84ca530d17789dfe6fbb0fceb4e6c3925d9
Angie K. Titova <angie.badfox@gmail.com>
2018-01-29 22:55:47 +0200

    [Bugfix] Fix JS by removing a call to unexisting function

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -7,7 +7,6 @@ const greet = require('./greeting.js')
 const particles = require('./particles.js')
 const carousel = require('./carousel.js')
 
-greet('🤦‍♂️');
 //particles('particles-js');
 carousel('.Wallop', true, true);
 carousel('.Reviews ', true, false);
