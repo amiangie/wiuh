c778681f383570da7f174f857fc5733bf2949be7
Angie K. Titova <angie.badfox@gmail.com>
2018-01-30 00:47:43 +0200

    [General] Enable particles

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,10 +3,9 @@
   Author: angie
  */
 
-const greet = require('./greeting.js')
 const particles = require('./particles.js')
 const carousel = require('./carousel.js')
 
-//particles('particles-js');
+particles('particles-js');
 carousel('.Wallop', true, true);
 carousel('.Reviews ', true, false);
