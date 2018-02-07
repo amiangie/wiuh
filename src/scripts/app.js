529cf8e094511e6ffef0b7bc3dbefcdd794cbcde
Angie K. Titova <angie.badfox@gmail.com>
2018-02-07 15:13:28 +0200

    [Animation] Add a library for appearing elements and make brand boxes appear on scroll too

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,9 +5,8 @@
 
 const particles = require('./particles.js')
 const carousel = require('./carousel.js')
-const appear = require('./appear.js')
+const carousel = require('./appear.js')
 
 particles('particles-js');
-appear('.js-services', '.js-card');
 carousel('.Wallop', true, true);
 carousel('.Reviews ', true, false);
--- src/scripts/appear.js
+++ src/scripts/appear.js
@@ -1,15 +1,55 @@
-const appear = function(section, ele) {
-  
-  window.onscroll = function(e) {
-    if ((window.innerHeight + window.scrollY) >= document.querySelector(section).offsetHeight) {
-      const elements = document.querySelectorAll(ele);
-      
-      for(let i = 0; i < elements.length; i++)
-        window.setTimeout(function() {
-          elements[i].classList.add('appear');
-        }, 300 * i);
-    }
-  };
-};
+//const appear = function(section, ele) {
+//  window.onscroll = function(e) {
+//    if ((window.innerHeight + window.scrollY) >= document.querySelector(section).offsetHeight) {
+//      const elements = document.querySelectorAll(ele);
+//      
+//      console.log(section)
+//      
+//      for(let i = 0; i < elements.length; i++)
+//        window.setTimeout(function() {
+//          elements[i].classList.add('appear');
+//        }, 300 * i);
+//    }
+//  };
+//};
+//
+//module.exports = appear;
 
-module.exports = appear;
+
+//const appear = function(section, ele) {
+//  window.onscroll = function(e) {
+//    if ((window.innerHeight + window.scrollY) >= document.querySelector(section).offsetHeight) {
+//      const elements = document.querySelectorAll(ele);
+//      
+//      console.log(section)
+//      
+//      for(let i = 0; i < elements.length; i++)
+//        window.setTimeout(function() {
+//          elements[i].classList.add('appear');
+//        }, 300 * i);
+//    }
+//  };
+//};
+//
+//module.exports = appear;
+
+appear({
+  init: function init(){
+    console.log('dom is ready');
+  },
+  elements: function elements(){
+    // work with all elements with the class "track"
+    return document.getElementsByClassName('js-appear');
+  },
+  appear: function appear(el){
+    const elements = el.querySelectorAll('.js-animate');
+    
+    for(let i = 0; i < elements.length; i++) {
+      window.setTimeout(function() {
+        elements[i].classList.add('appear');
+      }, 300 * i);
+    }
+  },
+  bounds: 200,
+  reappear: false
+});
