e29da9dbbe4f3cb90db1c80dd4e0855b2dbcc525
Angie K. Titova <angie.badfox@gmail.com>
2018-02-07 15:13:53 +0200

    [Cleanup] Remove unused JS

--- src/scripts/appear.js
+++ src/scripts/appear.js
@@ -1,38 +1,3 @@
-//const appear = function(section, ele) {
-//  window.onscroll = function(e) {
-//    if ((window.innerHeight + window.scrollY) >= document.querySelector(section).offsetHeight) {
-//      const elements = document.querySelectorAll(ele);
-//      
-//      console.log(section)
-//      
-//      for(let i = 0; i < elements.length; i++)
-//        window.setTimeout(function() {
-//          elements[i].classList.add('appear');
-//        }, 300 * i);
-//    }
-//  };
-//};
-//
-//module.exports = appear;
-
-
-//const appear = function(section, ele) {
-//  window.onscroll = function(e) {
-//    if ((window.innerHeight + window.scrollY) >= document.querySelector(section).offsetHeight) {
-//      const elements = document.querySelectorAll(ele);
-//      
-//      console.log(section)
-//      
-//      for(let i = 0; i < elements.length; i++)
-//        window.setTimeout(function() {
-//          elements[i].classList.add('appear');
-//        }, 300 * i);
-//    }
-//  };
-//};
-//
-//module.exports = appear;
-
 appear({
   init: function init(){
     console.log('dom is ready');
